eb20c9dfe004276c6ff536a98b91e854ff2793af
ankit-singh100 <asrankit456@gmail.com>
2025-07-24 22:17:03 +0545

    created user schema

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,11 +1,24 @@
-export const signup = async(req, res) => {
-  res.send("This is the sign up route");
-}
+import User from "../model/user.schema.js";
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+    res.status(201).json({ user, message: "User created successfully", user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: error.message });
+  }
+};
 
-export const login = async(req, res) => {
+export const login = async (req, res) => {
   res.send("This is the login route");
-}
+};
 
-export const logout = async(req, res) => {
+export const logout = async (req, res) => {
   res.send("This is the logout route");
-}
+};
